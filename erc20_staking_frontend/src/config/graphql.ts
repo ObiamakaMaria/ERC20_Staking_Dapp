2eb3814766d0dc013919558b7dc1daf80324c157
obiim <obiim@xubuntu.myguest.virtualbox.org>
2025-03-22 13:10:08 +0100

    BREAKING CHANGE! : deleted frontend dir

--- erc20_staking_frontend/src/config/graphql.ts
+++
@@ -1,5 +0,0 @@
-import { GraphQLClient } from 'graphql-request'
-
-export const graphqlClient = new GraphQLClient(
-  import.meta.env.VITE_SUBGRAPH_URL || 'http://localhost:8000/subgraphs/name/your-subgraph'
-) 
